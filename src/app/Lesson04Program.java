e98d0d31b3a7f8426fdc3969363635347c9d770a
AndreyKhrypach <pacik78@gmail.com>
2024-09-24 21:09:58 +0300

    Lesson information

---
+++ src/app/Lesson04Program.java
@@ -0,0 +1,90 @@
+package app;
+
+import java.util.Random;
+
+public class Lesson04Program {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int sum = 177;
+        byte players = 5;
+        System.out.println((double)sum / players);
+
+//        double debtUsa = 32.332273;
+//        long debt = (long)(debtUsa * 1_000_000_000_000L);
+//        System.out.println(debt);
+
+
+//        double x = 3.0465745;
+//        double y = 0.968032;
+//        System.out.println(Math.round(x));
+//        System.out.println(Math.round(y));
+//
+//        System.out.println(Math.ceil(x));
+//        System.out.println(Math.ceil(y));
+//
+//        System.out.println(Math.floor(x));
+//        System.out.println(Math.floor(y));
+
+//        int a = -18;
+//        a = Math.abs(a);
+//        //a *= -1;
+//        System.out.println(a);
+
+//        int num1 = 8;
+//        long res = ++num1 * 5;
+//        System.out.println(res);
+
+//        int num1 = 12;
+//       // num1 = num1 +1;
+//        //num1 +=1;
+//        num1++;
+//        System.out.println(num1);
+
+//
+//        int num1 = 1;
+//        int num2 = 100;
+//        System.out.println(num2);
+//        num2 -= num1;
+//        //num2 = num2 - num1;
+//
+//       // num2= num2 + 1;
+//        // num2 +=1;
+//        System.out.println(num1);
+//        System.out.println(num2);
+//        int res =  num1 + num2;
+//        System.out.println(res);
+
+
+//        int num1 = -13;
+//        int num2 = 5;
+//        int res = num1 % num2;
+//        System.out.println("Result of ( % ) is " + res);
+
+//        int num = 5;
+//        // ASCII-код g є 103
+//        char sym = 'g';
+//        double res = sym % num;
+//        System.out.println("Result of ( % ) is " + res);
+//        double superResult = 100 / res;
+//        System.out.println(superResult);
+
+//        int a = random.nextInt(10) + 1;
+//        System.out.println(a);
+//        System.out.println(a % 2);
+
+
+//        int firstNumber = 25;
+//        int secondNumber = 6;
+//
+//        int sum = firstNumber + secondNumber;
+//        System.out.println(sum);
+//        int subtract = secondNumber - firstNumber;
+//        System.out.println(subtract);
+//        long multiply = firstNumber * secondNumber;
+//        System.out.println(multiply);
+//        int divide = firstNumber / secondNumber;
+//        System.out.println(divide);
+    }
+}
